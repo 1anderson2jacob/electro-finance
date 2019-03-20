da1e6c8f91dfacf11e745e0b335e9477b7c04d4e
MichaelJahns <jahnsmic001@gmail.com>
2019-03-20 12:12:21 -0700

    Client can ping server without a fuss, need to figure out redirect next

--- server.js
+++ server.js
@@ -6,26 +6,32 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {
-
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
+app.get('/', (req, res) => { });
 app.get('/login', (req, res) => {
-  res.redirect('https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000&client_id=MICHAEL@AMER.OAUTHAP');
+  res.redirect(
+    'https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000&client_id=MICHAEL@AMER.OAUTHAP'
+  );
 });
 
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
   res.status(404);
   res.statusMessage = 'Not found';
 });
 
-app.use( (err, req, res, next) => {
+app.use((err, req, res, next) => {
   res.status(500);
   res.statusMessage = 'Server Error';
 });
 
 module.exports = {
   server: app,
-  start: (port) => {
+  start: port => {
     app.listen(port, () => console.log(`Server up on port ${port}`));
   },
 };
